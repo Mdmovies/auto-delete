af5142e21e3c745c6776a27a6bfc2a335c19fa97
Mdmovies <91911991+Mdmovies@users.noreply.github.com>
2022-02-26 16:30:52 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $BRANCH ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Mdmovies/auto-delete /auto-delete
+else
+  echo "Cloning $UPSTREAM_REPO branch from Respository"
+  git clone https://github.com/Mdmovies/auto-delete -b $BRANCH /auto-delete
+fi
+cd /auto-delete
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
