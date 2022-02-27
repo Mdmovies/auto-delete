a7a8e6382e77da4d301758f56a44a6a5fca8379e
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-27 12:42:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,8 +1,25 @@
 from os import environ 
+from database import db 
+from pyrogram.types import Message 
 
+GROUPS = []
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
 DATABASE = environ.get("DATABASE")
 SESSION = environ.get("SESSION")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL"))
+
+async def is_chat(_, bot, message: Message):
+    chat_id = message.chat.id
+    xx = await db.get_settings(chat_id)
+    if not await db.is_served_chat(chat_id):
+      return False         
+    if not xx["auto_delete"]:
+      return False
+    if not xx["bots"]:
+      return False 
+    if not int(chat_id) in GROUPS:
+       GROUPS.append(int(chat_id))
+    return True
+filters.chats=filters.create(is_chat)
