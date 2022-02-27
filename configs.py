560024e3f0c5353ac21ff0cbb6559b49af4f8a7e
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-27 14:03:07 +0530

    Better

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 from os import environ 
 from database import db 
-from pyrogram.types import Message 
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 GROUPS = []
 API_ID = int(environ.get("API_ID"))
@@ -22,4 +22,18 @@ async def is_chat(_, bot, message: Message):
     if not int(chat_id) in GROUPS:
        GROUPS.append(int(chat_id))
     return True
-filters.chats=filters.create(is_chat)
+    
+async def buttons(chat):
+   settings = await db.get_settings(chat)
+   if settings is not None:
+      button=[[
+         InlineKeyboardButton(f'Auto delete 🗑️', callback_data =f"done#auto_delete#{settings['auto_delete']}"), InlineKeyboardButton('OFF ❌' if settings['auto_delete'] else 'ON ✅', callback_data=f"done_#auto_delete#{settings['auto_delete']}")
+         ],[ 
+         InlineKeyboardButton(f'Timer 🕐', callback_data =f"done#time#{settings['time']}"), InlineKeyboardButton('OFF ❌' if settings['time'] else 'ON ✅', callback_data=f"done_#time#{settings['time']}")
+         ],[
+         InlineKeyboardButton(f'Delete Mode ⚙️', callback_data =f"done#mode#{settings['mode']}"), InlineKeyboardButton('OFF ❌' if settings['mode'] else 'ON ✅', callback_data=f"done_#mode#{settings['mode']}")
+         ],[
+         InlineKeyboardButton(f'Ignore admins 👱', callback_data =f"done#admins#{settings['admins']}"), InlineKeyboardButton('OFF ❌' if not settings['admins'] else 'ON ✅', callback_data=f"done_#admins#{settings['admins']}")
+      ]]
+   return InlineKeyboardMarkup(button)
+  
