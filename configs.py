de81c6d07ae55da16520f8d57e7e861c118266a5
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-27 14:06:23 +0530

    Avoid circular path

--- configs.py
+++ configs.py
@@ -6,7 +6,6 @@ GROUPS = []
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
-DATABASE = environ.get("DATABASE")
 SESSION = environ.get("SESSION")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL"))
 
