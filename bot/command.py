99684c7ed34a367c0f4540760b3f7a8cd9ac8acb
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-03-13 13:15:34 +0530

    Update command.py

--- bot/command.py
+++ bot/command.py
@@ -29,7 +29,7 @@ async def start(bot, cmd):
     
 #GROUPS = -1001531562598
 try:
-  @User.on_message(filters.chat(GROUPS) & service_filter)#filters.text & filters.group & filters.incoming & filters.chats)
+  @User.USER.on_message(filters.chat(GROUPS) & service_filter)#filters.text & filters.group & filters.incoming & filters.chats)
   async def user_client(bot, message):
       return await delete(bot, message)
 except:# UserNotParticipant:
