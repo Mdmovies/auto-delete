5d5f154a0e5acd4487c016862aaae0218bb37271
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-03-14 18:06:50 +0530

    Update

--- bot/command/deleteall.py
+++ bot/command/deleteall.py
@@ -1,12 +1,91 @@
 import time
 import asyncio 
-import datetime
+import datetime 
+from typing import List
 from bot.main import User, Bot
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+TG_MAX_SEL_MESG = 99
+TG_MIN_SEL_MESG = 0
+
+async def get_messages(
+    client: Bot,
+    chat_id: int,
+    min_message_id: int,
+    max_message_id: int,
+    filter_type_s: List[str],
+    status
+):
+    total = 0
+    error = 0
+    deleted = 0
+    messages_to_delete = []
+    start_time = time.time()
+    async for msg in client.iter_history(
+        chat_id=chat_id,
+        limit=None
+    ):
+        total+=1
+        if (
+            min_message_id <= msg.message_id and
+            max_message_id >= msg.message_id
+        ):
+            if len(filter_type_s) > 0:
+                for filter_type in filter_type_s:
+                    obj = getattr(msg, filter_type)
+                    if obj:
+                        messages_to_delete.append(msg.message_id)
+            else:
+                if msg.message_id != status.message_id:
+                    messages_to_delete.append(msg.message_id)
+        feched = len(messages_to_delete)
+        if feched > TG_MAX_SEL_MESG:
+            await mass_delete_messages(
+                client,
+                chat_id,
+                messages_to_delete
+            )
+            delete+=int(feched)
+            messages_to_delete = []
+            await status.edit(f"completed : {total}\ndeleted : {delete}\nerror : {total - delete}")
+    unknown = len(messages_to_delete)
+    if unknown > TG_MIN_SEL_MESG:
+        await mass_delete_messages(
+            client,
+            chat_id,
+            messages_to_delete
+        )
+        delete+=int(unknown)
+        messages_to_delete = []
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await status.edit(f"sucessful\nTime taken : {time_taken}\ncompleted : {total}\ndeleted : {delete}\nerror : {total - delete}")
+    return True 
+   
+async def mass_delete_messages(
+    client: Bot,
+    chat_id: int,
+    message_ids: List[int]
+):
+    return await client.delete_messages(
+        chat_id=chat_id,
+        message_ids=message_ids,
+        revoke=True
+    )
 @Client.on_message(filters.command(["del", "delete"]) & filters.group)
 async def delete_all(bot, message):
+   msg = await message.reply_text("please wait it take some time to finish")
+   await get_messages(
+        bot.USER,
+        message.chat.id,
+        0,
+        msg.message_id if msg else message.message_id,
+        [],
+        msg
+    )
+   return 
+
+async def dumb(client, message):
    chat = message.chat.id
    froms = message.message_id
    total = 0
@@ -44,12 +123,12 @@ async def delete_all(bot, message):
             error+=1
             continue
            
-   time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+   
    try:
      await bot.send_message(chat, text=f"ids\n{MSG_ID}")
    except:
       print(MSG_ID)
-   await msg.edit(f"**completed**\n complete in : {time_taken}\nTotal messages : {current}\ndelete sucessful : {sucessful}\ndelete unsucessful : {error}\n\nalready deleted : {deleted}")
+ #  await msg.edit(f"**completed**\n complete in : {time_taken}\nTotal messages : {current}\ndelete sucessful : {sucessful}\ndelete unsucessful : {error}\n\nalready deleted : {deleted}")
          
        
          
