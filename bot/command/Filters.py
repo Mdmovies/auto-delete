517b547385fbe254888b6f4fc2ffbc68a9e81d14
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-03-15 16:35:36 +0530

    update

--- bot/command/Filters.py
+++ bot/command/Filters.py
@@ -1,5 +1,6 @@
 import asyncio 
 from database import db 
+from .command import save_settings
 from pyrogram import Client, filters 
 
 @Client.on_message(filters.command('whitelist') & filters.group)
@@ -7,57 +8,83 @@ async def whitelist(client, message):
   chat = message.chat.id
   if not message.reply_to_message:
     if message.text < 2:
-      return await message.reply_text("give a user id")
+      return await message.reply_text("give me a user id")
     i, user_id = await message.split(None, 1)
   else:
     user_id = message.reply_to_message.from_user.id
+  try:
+     user = await client.get_users(user_id)
+  except Exception as e:
+        return await message.reply(f'Error - {e}')
   add = await db.add_whitelist(user_id, chat)
   if not add:
-    await message.reply_text("given user already in whitelist")
+    await message.reply_text(f"{user.mention} already in whitelist")
   else:
-    await message.reply_text(f"Added {user_id} to whitelist")
+    await message.reply_text(f"Successfully Added {user.mention} to whitelist")
     
 @Client.on_message(filters.command('rwhitelist') & filters.group)
 async def rwhitelist(client, message):
   chat = message.chat.id
   if not message.reply_to_message:
     if message.text < 2:
-      return await message.reply_text("give a user id")
+      return await message.reply_text("give me a user id")
     i, user_id = await message.split(None, 1)
   else:
     user_id = message.reply_to_message.from_user.id
+  try:
+     user = await client.get_users(user_id)
+  except Exception as e:
+        return await message.reply(f'Error - {e}')
   add = await db.remove_whitelist(user_id, chat)
   if not add:
-    await message.reply_text("given user not in whitelist")
+    await message.reply_text(f"{user.mention} not in whitelist")
   else:
-    await message.reply_text(f"{user_id} removed from whitelist")
+    await message.reply_text(f"Successfully {user.mention} removed from whitelist")
     
 @Client.on_message(filters.command('blacklist') & filters.group)
 async def blacklist(client, message):
   chat = message.chat.id
   if not message.reply_to_message:
     if message.text < 2:
-      return await message.reply_text("give a user id")
+      return await message.reply_text("give me a user id")
     i, user_id = await message.split(None, 1)
   else:
     user_id = message.reply_to_message.from_user.id
+  try:
+     user = await client.get_users(user_id)
+  except Exception as e:
+        return await message.reply(f'Error - {e}')
   add = await db.add_blacklist(user_id, chat)
   if not add:
-    await message.reply_text("given user already in blacklist")
+    await message.reply_text(f"{user.mention} already in blacklist")
   else:
-    await message.reply_text(f"Added {user_id} to blacklist")
+    await message.reply_text(f"Successfully {user.mention} Added to blacklist")
     
 @Client.on_message(filters.command('rblacklist') & filters.group)
 async def rblacklist(client, message):
   chat = message.chat.id
   if not message.reply_to_message:
     if message.text < 2:
-      return await message.reply_text("give a user id")
+      return await message.reply_text("give me a user id")
     i, user_id = await message.split(None, 1)
   else:
     user_id = message.reply_to_message.from_user.id
+  try:
+     user = await client.get_users(user_id)
+  except Exception as e:
+        return await message.reply(f'Error - {e}')
   add = await db.remove_blacklist(user_id, chat)
   if not add:
-    await message.reply_text("given user not in blacklist")
+    await message.reply_text(f"{user.mention} not in blacklist")
   else:
-    await message.reply_text(f"{user_id} removed from blacklist") 
+    await message.reply_text(f"Successfully {user.mention} removed from blacklist") 
+
+@Client.on_message(filters.command('time') & filters.group)
+async def time(client, message):
+  chat = message.chat.id
+  if message.command < 1:
+      return await message.reply_text("give me a time in seconds ! eg: /time 100")
+  time = message.command[1]
+  await save_settings(chat, "time", time)
+  await message.reply_text(f"Successfully Time changed to **{time}s**")
+  return
