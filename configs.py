646531d08699e87387dff89739132cf33f8784ea
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-03-15 14:33:27 +0530

    Settings

--- configs.py
+++ configs.py
@@ -4,6 +4,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 class temp(object):
    GROUPS = []
+   SETTINGS = {}
    API_ID = int(environ.get("API_ID"))
    API_HASH = environ.get("API_HASH")
    BOT_TOKEN = environ.get("BOT_TOKEN")
