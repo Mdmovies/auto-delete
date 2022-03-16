a3e195240ea622fe183d4c654d5d91c1928b835f
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-03-16 17:58:10 +0530

    Seperate

---
+++ bot/main.py
@@ -0,0 +1,17 @@
+from configs import temp
+from pyrogram import Client
+
+User = Client(session_name=temp.SESSION,
+              api_id=temp.API_ID,
+              api_hash=temp.API_HASH,
+              workers=300
+              )
+
+Bot = Client(session_name="auto-deletes",
+             api_id=temp.API_ID,
+             api_hash=temp.API_HASH,
+             bot_token=temp.BOT_TOKEN,
+             plugins={"root": "bot/command"},
+             sleep_threshold=5,
+             workers=300
+             )
