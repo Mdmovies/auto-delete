696b920bc3abc2e7951fdc9274b5f85ccb117218
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-03-26 22:49:18 +0530

    Fix

--- configs.py
+++ configs.py
@@ -21,13 +21,11 @@ async def is_chat(_, bot, message: Message):
     m = message
     text = message.text
     get = await db.get_settings(m.chat.id)
-    if get["mode"]=="whitelist":
-        print(m.from_user.id)
-        if await db.in_whitelist(m.chat.id, m.from_user.id):
+    if get["mode"]=="whitelist" and await db.in_whitelist(m.from_user.id, m.chat.id):
           return False 
-    elif get["mode"]=="blacklist" and not (await db.in_blacklist(m.chat.id, m.from_user.id)):
+    elif get["mode"]=="blacklist" and not (await db.in_blacklist(m.from_user.id, m.chat.id)):
         return False 
-    elif get["mode"]=="bots" and (m.from_user.is_bot):
+    elif get["mode"]=="bots" and m.from_user.is_bot:
         return False
     if message.from_user.id == temp.bot_id:
        return False
