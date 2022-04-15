d47ee8a5cb065af0c0931df2577c2b70da324c52
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-15 11:34:57 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -12,6 +12,7 @@ Bot = Client(session_name="auto-deletes",
              api_id=temp.API_ID,
              api_hash=temp.API_HASH,
              bot_token=temp.BOT_TOKEN,
+             plugins={"root": "bot/command"},
              sleep_threshold=5,
              workers=300
              )
