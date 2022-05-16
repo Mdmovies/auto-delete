d0d9868487c6a5f605e618e5533ff5b433a59723
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-05-17 01:34:51 +0530

    Test

---
+++ bot/command/clone.py
@@ -0,0 +1,71 @@
+import os 
+import sys
+import time
+import heroku3 
+from bot.main import Bot 
+from pyrogram import filters
+
+TOKEN = "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" 
+
+@Bot.on_message(filters.command('create') & filters.private)
+async def create_clone(bot, message):
+ try:
+  app = {
+     "region": "eu",
+  }
+  source_blob = {}
+  overrides = {"env": {}}
+  user = message.from_user
+  heroku_api = await bot.ask(chat_id=user.id, text="Please give your heroku api key")
+  if heroku_api.text == "/cancel":
+      return await message.reply('**process cancelled**')
+  api_key = heroku_api
+  heroku_app_name = await bot.ask(chat_id=user.id, text='please give a app name')
+  if heroku_app_name.text == "/cancel":
+      return await message.reply('**process cancelled**')
+  app_name = heroku_app_name
+  app_type = "production"
+  domain = ".example.com"
+  reference_app = "reference-app"
+  app["name"] = "example-{0}".format(app_name)
+  git_name = "heroku_live"
+  heroku_conn = heroku3.from_key(api_key)
+  overrides["env"]["DJANGO_HOSTNAME"] = "{0}{1}".format(app_name, domain)
+  heroku_app = heroku_conn.app(reference_app)
+  config = heroku_app.config()
+  source_blob["url"] = "https://api.github.com/repos/example/{0}/tarball/master?access_token={1}".format(
+        app_name, TOKEN
+  )
+  data = {
+        "app": app,
+        "source_blob": source_blob,
+        "overrides": overrides,
+    }
+  heroku_appsetup = heroku_conn.create_appsetup(**data)
+  app = heroku_conn.app(heroku_appsetup.app.id)
+  time_checkpoint = time.time
+  waiting = True
+  while True: 
+      success, time_checkpoint = wait_for_status_event(heroku_conn, heroku_appsetup.id, time_checkpoint)
+      if success:
+          break 
+  appsetup = heroku_conn.get_appsetup(heroku_appsetup.id)
+  if appsetup.status == "succeeded":
+      print("Enabling heroku labs runtime dyno metrics")
+      app.enable_feature("runtime-dyno-metadata")
+      await message.reply("dyno enabled")
+ except Exception as e:
+  await message.reply(e)
+  
+def wait_for_status_event(heroku_conn, appsetup_id, time_checkpoint):
+    time_check = time.time()
+    if (time_check - time_checkpoint) > 5:
+        time_checkpoint = time_check
+        print("checking status", end="\r")
+        appsetup = heroku_conn.get_appsetup(appsetup_id)
+        if appsetup.build:
+            print("Build Available", appsetup.build)
+            return True, time_checkpoint
+        if appsetup.status in ["failed", "succeeded"]:
+            return True, time_checkpoint
+    return False, time_checkpoint
